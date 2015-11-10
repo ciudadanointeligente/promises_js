59e830d3003fa8202191338343d9eab30e026c08
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-11-10 19:15:31 -0300

    Adding files for protractor testing

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    protractor: {
+      options: {
+        configFile: "protractor_config.js", // Default config file 
+        keepAlive: true, // If false, the grunt process stops when the test fails. 
+        noColor: false, // If true, protractor will not use colors in its output. 
+        args: {
+          // Arguments passed to the command 
+        }
+      },
+      promises: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too. 
+        options: {
+          //configFile: "e2e.conf.js", // Target-specific config file 
+          args: {} // Target-specific arguments 
+        }
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'protractor']);
+
+};
---
+++ specs/promises.js
@@ -0,0 +1,7 @@
+describe('initial', function() {
+  it('true is true', function() {
+    // Load the AngularJS homepage.
+
+    expect(true).toBe(true);
+  });
+});
